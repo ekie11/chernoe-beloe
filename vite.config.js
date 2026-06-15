a4eb3e411b3b9cff41880686af9bf6d49e14e565
ekie11 <harhanovk@gmail.com>
2026-06-15 21:03:43 +0300

    base для Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
+  base: "/chernoe-beloe/",
   plugins: [react(), tailwindcss()],
 })
